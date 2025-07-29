311b286bfd7cc51c69bfa850d72a573e712e308f
Debaprasad Tapader <dev.openroot@gmail.com>
2025-07-29 23:50:36 +0530

    update

---
+++ private/public/configuration.php
@@ -0,0 +1,20 @@
+<?php
+	$configuration = [
+		"mysql1" => [
+			"credential" => [
+				"host" => "",
+				"port" => "",
+				"username" => "",
+				"password" => "",
+				"DB1" => [
+					"name" => ""
+				],
+				"ssl" => [
+					"mode" => "",
+					"caCertificatePath" => ""
+				],
+				"uri" => ""
+			]
+		]
+	];
+?>
